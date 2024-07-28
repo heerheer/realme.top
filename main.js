ccd07f669281f1d542a3a0ddaadd94295ea36fc0
heer <1767407822@qq.com>
2024-07-28 10:55:54 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+import Typewriter from 'typewriter-effect/dist/core';
+
+
+
+new Typewriter('#type_text', {
+  strings: ['Side', '骚货'],
+  autoStart: true,
+  loop: true, 
+  deleteSpeed:300
+});
